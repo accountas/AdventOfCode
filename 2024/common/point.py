260065ec535121ddf10b5beeeca13f41ab36a389
accountas <martynas.cibulskis@vinted.com>
2024-12-06 08:30:51 +0200

    optimize a bit

--- 2024/common/point.py
+++ 2024/common/point.py
@@ -1,8 +1,13 @@
+from typing import Any
+
+
 class Point:
     def __init__(self, *values):
         self.values = values
-        self._index_alias = {"i": 0, "j": 1, "k": 2, "x": 0, "y": 1, "z": 2}
-        self._create_attributes()
+
+        for name, idx in [("i", 0), ("j", 1), ("k", 2), ("x", 0), ("y", 1), ("z", 2)]:
+            if idx < len(values):
+                setattr(self, name, values[idx])
 
     def __repr__(self):
         return f"Point({', '.join(map(str, self.values))})"
@@ -29,14 +34,3 @@ class Point:
 
     def __hash__(self) -> int:
         return hash(tuple(self.values))
-
-    def _create_attributes(self):
-        for name, idx in self._index_alias.items():
-
-            def getter(self, _idx=idx):
-                return self.values[_idx]
-
-            def setter(self, value, _idx=idx):
-                self.values[_idx] = value
-
-            setattr(self.__class__, name, property(getter, setter))
