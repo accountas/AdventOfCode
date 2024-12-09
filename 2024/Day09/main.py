64c83fb950def249fd2cc3f3514f0efc45f5327a
accountas <martynas.cibulskis@vinted.com>
2024-12-09 08:57:09 +0200

    clean up day 09

--- 2024/Day09/main.py
+++ 2024/Day09/main.py
@@ -4,61 +4,76 @@ from dataclasses import dataclass
 from typing import Optional
 
 
-def add_ids(disk):
-    return [(int(size), [None, idx // 2][idx % 2 == 0]) for idx, size in enumerate(disk)]
+@dataclass
+class File:
+    file_id: Optional[int]
+    size: int
+    was_moved: bool = False
 
-def decompact(data):
-    full = []
-    for i in data:
-        full += [i[1]] * i[0]
-    return full
 
-def part_1(disk):
-    disk = decompact(add_ids(disk))
-    checksum = 0
-    l = 0
-    r = len(disk) - 1
-    while l <= r:
-        if disk[l] != None:
-            checksum += l * disk[l]
-            l += 1
-        elif disk[r] != None:
-            checksum += l * disk[r]
-            r -= 1
-            l += 1
+def to_files(disk_map):
+    return [
+        File(file_id=idx // 2 if idx % 2 == 0 else None, size=int(size))
+        for idx, size in enumerate(disk_map)
+    ]
+
+
+def to_blocks(files):
+    blocks = []
+    for file in files:
+        blocks += [file.file_id] * file.size
+    return blocks
+
+
+def checksum(blocks):
+    return sum(idx * file_id for idx, file_id in enumerate(blocks) if file_id != None)
+
+
+# O(n * max_file_size)
+def part_1(files): 
+    blocks = to_blocks(files)
+    left = 0
+    right = len(blocks) - 1
+    while left <= right:
+        if blocks[left] != None:
+            left += 1
+        elif blocks[right] != None:
+            blocks[left] = blocks[right]
+            blocks[right] = None
+            right -= 1
+            left += 1
         else:
-            r -= 1
-    return checksum
+            right -= 1
+    return checksum(blocks)
 
+# O(n ^ 2) :(
+def part_2(files):
+    for i in tqdm(range(len(files) - 1, 0, -1)):
+        file_to_move = files[i]
 
-def part_2(disk):
-    disk = add_ids(disk)
-
-    for i in tqdm(range(len(disk) - 1, 0, -1)):
-        if disk[i][1] is None or len(disk[i]) == 3:
+        if file_to_move.file_id is None or file_to_move.was_moved:
             continue
 
-        disk[i] = (disk[i][0], disk[i][1], True)
+        file_to_move.was_moved = True
 
         for j in range(0, i):
-            if disk[j][1] is None and disk[i][0] <= disk[j][0]:
-                tmp = disk[i]
-                disk[i] = (tmp[0], None)
-                disk = disk[:j] + [tmp] + [(disk[j][0] - tmp[0], None)] + disk[j+1:]
-    
-    hash = 0
-    for idx, file_id in enumerate(decompact(disk)):
-        if file_id != None:
-            hash += file_id * idx
-    return hash
+            move_to = files[j]
+            if move_to.file_id is None and move_to.size >= file_to_move.size:
+                files[i] = File(None, file_to_move.size)
+                remaining = File(None, move_to.size - file_to_move.size)
+                files = files[:j] + [file_to_move, remaining] + files[j + 1 :]
+                break
 
-    
+    return checksum(to_blocks(files))
+
+
 def main():
-    input = get_data(year=2024, day=9, sample=False)
-    print(f"N = {len(input)}")
+    disk_map = get_data(year=2024, day=9, sample=False)
+    files = to_files(disk_map)
 
-    print(f"Part 1: {part_1(input)}")
-    print(f"Part 2: {part_2(input)}")
+    print(f"Part 1: {part_1(files)}")
+    print(f"Part 2: {part_2(files)}")
+
 
 if __name__ == "__main__":
     main()
