47fecdd91723f73ffb00331fee6569b73e91b52e
accountas <martynas.cibulskis@vinted.com>
2024-12-12 07:36:05 +0100

    Clean up day 12

--- 2024/Day12/main.py
+++ 2024/Day12/main.py
@@ -2,86 +2,85 @@ from common.data import *
 from common.grid import *
 from common.point import *
 from collections import defaultdict
-from copy import copy
 
-def compute_sides(plot):
+
+def fill_plot(pos, garden, plant_type, plot_id, visited):
+    if pos in visited:
+        return 0, 0
+
+    area = 1
+    perimeter = 4 - len(list(garden.neighbours(pos)))
+
+    garden[pos] = plot_id
+    visited.add(pos)
+
+    for child in garden.neighbours(pos):
+        if garden[child] == plant_type:
+            result = fill_plot(child, garden, plant_type, plot_id, visited)
+            area += result[0]
+            perimeter += result[1]
+        elif garden[child] != plot_id:
+            perimeter += 1
+
+    return area, perimeter
+
+
+def compute_sides(garden):
+    # Scan dir, step dir, side dir
     dirs = [
         (RIGHT, DOWN, UP),
         (RIGHT, DOWN, DOWN),
         (DOWN, RIGHT, LEFT),
-        (DOWN, RIGHT, RIGHT)
+        (DOWN, RIGHT, RIGHT),
     ]
 
-    pers = defaultdict(lambda:0)
-    for dir_a, dir_b, dir_c in dirs:
-        pos = Point(0, 0)
-        end = Point(plot.n - 1, plot.m - 1)
-        while plot.is_valid(pos):
-            plant_last = plot[pos]
-            is_side_last = plot[pos] != plot[pos + dir_c]
-            pos_start = pos
-            while plot.is_valid(pos):
-                pos += dir_a
-                plant_now = plot[pos]
-                is_side_now = plot[pos] != plot[pos + dir_c]
+    perimeters = defaultdict(lambda: 0)
+    for scan_dir, step_dir, side_dir in dirs:
+        cur_pos = Point(0, 0)
+        while garden.is_valid(cur_pos):
+            plant_last = garden[cur_pos]
+            is_side_last = garden[cur_pos] != garden[cur_pos + side_dir]
+            next_line_start = cur_pos + step_dir
+
+            while garden.is_valid(cur_pos):
+                cur_pos += scan_dir
+                plant_now = garden[cur_pos]
+                is_side_now = garden[cur_pos] != garden[cur_pos + side_dir]
 
                 if is_side_last and (plant_last != plant_now or not is_side_now):
-                    pers[plant_last] += 1
-                plant_last = plant_now
-                is_side_last = is_side_now
-            pos = pos_start + dir_b
-    return pers
+                    perimeters[plant_last] += 1
+
+                plant_last, is_side_last = plant_now, is_side_now
+            cur_pos = next_line_start
+    return perimeters
 
 
 def main():
     input = get_data(year=2024, day=12, sample=False)
-    plot = Grid.from_string(input)
+    garden = Grid.from_string(input)
 
     visited = set()
-
-    def dfs(pos, plant, plot_idx):
-        if pos in visited:
-            return 0, 0
-        visited.add(pos)
-        plot[pos] = plot_idx
-
-        area = 1
-        per = 4 - len(list(plot.neighbours(pos)))
-
-        for neigh in plot.neighbours(pos):
-            if plot[neigh] == plant:
-                a, b = dfs(neigh, plant, plot_idx)
-                area += a
-                per += b
-            else:
-                per += 1
-        return area, per
-    
-    plot_size = {}
-    plot_per = {}
-    num_plots = 0
-    for pos, plant in plot.items():
+    sizes = {}
+    perimeters = {}
+    plot_count = 0
+    for pos, plant in garden.items():
         if pos not in visited:
-            a, b = dfs(pos, plant, num_plots)
-            plot_size[num_plots] = a
-            plot_per[num_plots] = b
-            num_plots += 1
+            result = fill_plot(pos, garden, plant, plot_count, visited)
+            sizes[plot_count] = result[0]
+            perimeters[plot_count] = result[1]
+            plot_count += 1
 
-    plot_sides = compute_sides(plot)
-
+    sides = compute_sides(garden)
 
     part_1 = 0
     part_2 = 0
 
-    for plot in range(num_plots):
-        part_1 += plot_size[plot] * plot_per[plot]
-        part_2 += plot_sides[plot] * plot_size[plot]
-    
+    for plot_id in range(plot_count):
+        part_1 += sizes[plot_id] * perimeters[plot_id]
+        part_2 += sizes[plot_id] * sides[plot_id]
 
-    print(part_1)
-    print(part_2)
-
-
+    print("Part 1:", part_1)
+    print("Part 2:", part_2)
 
 
 if __name__ == "__main__":
