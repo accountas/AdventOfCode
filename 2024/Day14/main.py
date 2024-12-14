b6c118efcb3d3083eead789570e5dbf370e730e6
accountas <martynas.cibulskis@vinted.com>
2024-12-14 09:40:07 +0200

    Add alternative solution

--- 2024/Day14/main.py
+++ 2024/Day14/main.py
@@ -33,7 +33,7 @@ def simulate(robots, steps, width, height):
     return after
 
 
-def count_quadrants(robots, width, height):
+def quadrant_score(robots, width, height):
     quadrants = [0] * 4
 
     def side(pos, length):
@@ -50,7 +50,7 @@ def count_quadrants(robots, width, height):
         if side_x is not None and side_y is not None:
             quadrants[side_x + side_y * 2] += 1
 
-    return quadrants
+    return math.prod(quadrants)
 
 
 def print_state(robots, width, height):
@@ -58,7 +58,6 @@ def print_state(robots, width, height):
     for pos, _ in robots:
         grid[pos.y, pos.x] = "#"
     print(grid)
-
 
 
 def largest_blob(robots):
@@ -83,19 +82,29 @@ def main():
     input = get_data(year=2024, day=14, sample=False)
     robots = parse_input(input)
     width, height = 101, 103
+    max_iterations = 20000
 
-    part_1 = math.prod(count_quadrants(simulate(robots, 100, width, height), width, height))
-    part_2 = None
+    print("Part 1:", quadrant_score(simulate(robots, 10000, width, height), width, height))
 
-    for steps in tqdm(range(0, 10000)):
+    for steps in tqdm(range(max_iterations), "Part 2, blob size"):
         after = simulate(robots, steps, width, height)
         if largest_blob(after) > 50:
             print_state(after, width, height)
             part_2 = steps
             break
 
-    print("part 1", part_1)
-    print("part 2", part_2)
+    print("Part 2:", part_2)
+
+    # Alternative solution, small part 1 score = quadrants unevenly distributed
+    # A lot faster than the above solution
+    min_quadrant_score = (1e9, None)
+    for steps in tqdm(range(max_iterations), "Part 2, min quadrant score"):
+        after = simulate(robots, steps, width, height)
+        score = quadrant_score(after, width, height)
+        min_quadrant_score = min(min_quadrant_score, (score, steps))
+
+    print_state(simulate(robots, min_quadrant_score[1], width, height), width, height)
+    print("Part 2:", part_2)
 
 
 if __name__ == "__main__":
