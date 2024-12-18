a0d8d47bc124ade6515ca0f75d80b5071266e007
accountas <martynas.cibulskis@vinted.com>
2024-12-18 14:01:15 +0200

    Optimize with DSU

--- 2024/Day18/main.py
+++ 2024/Day18/main.py
@@ -5,14 +5,44 @@ from collections import deque
 from tqdm import tqdm
 
 
+class DSU:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+        elif self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.rank[x] < self.rank[y]:
+            x, y = y, x
+
+        self.parent[y] = x
+        if self.rank[x] == self.rank[y]:
+            self.rank[x] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
 def parse_input(input):
     return [Point(*list(map(int, line.split(",")[::-1]))) for line in input.splitlines()]
 
 
-def points_in_shortest_path(grid, start, end):
+def shortest_path(grid, start, end):
     queue = deque([start])
     dist = {start: 0}
-    prev = {start: None}
 
     while queue:
         current = queue.popleft()
@@ -22,19 +52,9 @@ def points_in_shortest_path(grid, start, end):
         for neighbor in grid.neighbours(current):
             if neighbor not in dist and grid[neighbor]:
                 dist[neighbor] = dist[current] + 1
-                prev[neighbor] = current
                 queue.append(neighbor)
 
-    if end not in dist:
-        return None
-
-    path = set()
-    node = end
-    while node is not None:
-        path.add(node)
-        node = prev.get(node)
-
-    return path
+    return dist.get(end)
 
 
 def main():
@@ -46,25 +66,33 @@ def main():
     grid = Grid.empty(71, 71, True)
     fall_first = 1024
 
+    # ====== PART 1 ======
     for point in points[:fall_first]:
         grid[point] = False
 
-    shortest_path = points_in_shortest_path(grid, start, end)
+    print("Part 1:", shortest_path(grid, start, end))
 
-    print("Part 1", len(shortest_path) - 1)
-
+    # ====== PART 2 ======
     for point in points[fall_first:]:
         grid[point] = False
-        if point in shortest_path:
-            shortest_path = points_in_shortest_path(grid, start, end)
-            a += 1
 
-        if shortest_path is None:
+    dsu = DSU()
+
+    def add_point(point):
+        for neighbor in grid.neighbours(point):
+            if grid[neighbor]:
+                dsu.union(point, neighbor)
+
+    for point in grid.find(True):
+        add_point(point)
+
+    for point in reversed(points):
+        grid[point] = True
+        add_point(point)
+        if dsu.connected(start, end):
             break
 
-    print(
-        f"Part 2: {point.j},{point.i}",
-    )
+    print(f"Part 2: {point.j},{point.i}")
 
 
 if __name__ == "__main__":
