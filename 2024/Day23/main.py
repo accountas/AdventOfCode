2d1e23ab60f67177be8c8e207de65b3745d28d69
accountas <martynas.cibulskis@vinted.com>
2024-12-23 09:29:16 +0200

    Clean up and optimize day 23

--- 2024/Day23/main.py
+++ 2024/Day23/main.py
@@ -3,12 +3,13 @@ from common.data import *
 from time import time
 from collections import defaultdict
 from itertools import combinations
+from random import shuffle
 
 
 def parse_input(input):
     graph = defaultdict(set)
-
     lines = input.splitlines()
+    shuffle(lines)
     for line in lines:
         a, b = line.split("-")
         graph[a].add(b)
@@ -16,49 +17,44 @@ def parse_input(input):
     return graph
 
 
-def subsets(iterable, max_size):
-    if max_size is None:
-        max_size = len(iterable)
-    else:
-        max_size = min(max_size, len(iterable))
-
-    for size in range(max_size, 0, -1):
-        for subset in combinations(iterable, size):
-            yield subset
-
-
-def is_clique(graph, nodes):
-    return all(b in graph[a] for a, b in combinations(nodes, 2))
+def count_triangles(graph):
+    trianges = set(
+        tuple(sorted([a, b, c]))
+        for a in graph if a[0] == "t"
+        for b in graph[a]
+        for c in graph[b] if c in graph[a]
+    )
+    return len(trianges)
 
 
-def find_cliques(graph, max_size=None, largest_only=False):
-    cliques = set()
+def max_clique(graph):
+    best = set()
+
+    def subsets(nodes):
+        for size in range(len(nodes), 0, -1):
+            for subset in combinations(nodes, size):
+                yield subset
+
+    def is_clique(nodes):
+        return all(b in graph[a] for a, b in combinations(nodes, 2))
 
     for node in graph:
-        for subset in subsets(graph[node], max_size):
+        for subset in subsets(graph[node]):
             subset = set(subset) | {node}
-            if is_clique(graph, subset):
-                cliques.add(tuple(sorted(subset)))
-                if largest_only:
-                    break
+            if len(subset) <= len(best):
+                break
+            if is_clique(subset):
+                best = subset
 
-    return cliques
+    return ",".join(sorted(best))
 
 
 def main():
     input = get_data(year=2024, day=23, sample=False)
     graph = parse_input(input)
 
-    cliques = find_cliques(graph, max_size=3)
-    triangles = sum(
-        len(clique) == 3 and any(n[0] == "t" for n in clique) 
-        for clique in cliques
-    )
-    print("Part 1:", triangles)
-
-    cliques = find_cliques(graph, largest_only=True)
-    largest = max(cliques, key=lambda x: len(x))
-    print("Part 2:", ",".join(largest))
+    print("Part 1:", count_triangles(graph))
+    print("Part 2:", max_clique(graph))
 
 
 if __name__ == "__main__":
